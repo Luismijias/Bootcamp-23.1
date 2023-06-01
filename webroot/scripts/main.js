69a864d3595f9bec6c1b7a64e7c0af80fe596cf1
Luismijias <luismijias@gmail.com>
2023-06-01 18:44:53 +0000

    Automatic commit

---
+++ webroot/scripts/main.js
@@ -0,0 +1,10 @@
+/* alert('Seja bem vindo pelo Alert');
+console.warn('Alert ..');
+console.log('Mensagem de log em geral');
+console.error('Aconteceu um erro!!!'); */
+
+/* const logo = document.querySelector('div.logo');
+logo.style.border = '2px solid yellow'; */
+
+
+
