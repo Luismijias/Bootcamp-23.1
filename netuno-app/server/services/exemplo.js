1e56798a523fa6096f8112a6ea585b99ce259753
Luismijias <luismijias@gmail.com>
2023-06-08 14:36:39 +0000

    Automatic commit

---
+++ netuno-app/server/services/exemplo.js
@@ -0,0 +1,21 @@
+_out.println('<h1>FRUTAS<h1>')
+
+const frutas = ['Laranja', 'Abacate', 'Manga']
+
+_out.println('<h3>')
+
+for (const fruta of frutas) {
+  _out.println('<p>' + fruta + '</p>')
+}
+
+_out.println('<h1>ANIMAIS<h1>')
+
+_out.println('<h3>')
+
+const animais = ['Macaco', 'Onça', 'Tamanduá', 'Arara']
+
+for (const animal of animais) {
+  _out.println(`<p>${animal}</p>`)
+}
+
+_out.println('<h1>TESTE DE EVETOS</h1>')
