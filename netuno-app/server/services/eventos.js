359fa0b5b36c74e63cec76bd30e9a5bc1becbcaa
Luismijias <luismijias@gmail.com>
2023-06-08 16:51:25 +0000

    Automatic commit

--- netuno-app/server/services/eventos.js
+++ netuno-app/server/services/eventos.js
@@ -1,21 +1,54 @@
-_out.println('<h1>FRUTAS<h1>')
+_out.println('<h1>Eventos</h1>')
+const dbEventos = _db.query(`
+SELECT
+  evento.nome AS "evento_nome",
+  evento.data AS "evento_data",
+  evento.hora AS "evento_hora",
+  categoria.nome AS "categoria_nome"
+  FROM evento
+    INNER JOIN categoria ON evento.categoria_id = categoria.id
+    `)
+_out.println (`
+<style>
+  .tabela-eventos__estilo--funk {
+   background-color: red;
+  }
+  .tabela-eventos__estilo--samba {
+   background-color: yellow;
+  }
+</style>
+`)
+_out.println('<table class="tabela-eventos" border="1" cellspacing="3" cellpadding="5">')
 
-const frutas = ['Laranja', 'Abacate', 'Manga']
+_out.println('<thead>')
+  _out.println (`
+  <tr>
+    <th>Evento</th>
+    <th>Data</th>
+    <th>Hora</th>
+    <th>Estilo</th>
+  </tr>
+  `)
+_out.println('</thead>')
 
-_out.println('<h3>')
+_out.println('<tbody>')
+for (const dbEvento of dbEventos) {
+  let classesCSS = ''
+  if (dbEvento.getString('categoria_nome') == 'Funk') {
+    classesCSS = 'tabela-eventos__estilo--funk'
+  }
+  else if (dbEvento.getString('categoria_nome') == 'Samba') {
+    classesCSS = 'tabela-eventos__estilo--samba'
+  }
+  
+  _out.println (`
+  <tr class="${classesCSS}">
+    <td>${dbEvento.getString('evento_nome')}</td>
+    <td>${dbEvento.getString('evento_data')}</td>
+    <td>${dbEvento.getString('evento_hora')}</td>
+    <td>${dbEvento.getString('categoria_nome')}</td>
+  </tr>
+`)}
+_out.println('</tbody>')
 
-for (const fruta of frutas) {
-  _out.println('<p>' + fruta + '</p>')
-}
-
-_out.println('<h1>ANIMAIS<h1>')
-
-_out.println('<h3>')
-
-const animais = ['Macaco', 'Onça', 'Tamanduá', 'Arara']
-
-for (const animal of animais) {
-  _out.println(`<p>${animal}</p>`)
-}
-
-_out.println('<h1>TESTE DE EVETOS</h1>')
+_out.println('</table>')
