c913968c14dddcec0471e735677689f86863ce59
Luismijias <luismijias@gmail.com>
2023-06-27 00:16:20 +0000

    Automatic commit

--- netuno-app/server/services/evento/list/post.js
+++ netuno-app/server/services/evento/list/post.js
@@ -4,16 +4,19 @@ const numeroPagina = _req.getValues('pagina', _val.map()).getInt('numero', 1)
 const PAGE_SIZE = 2 
 const SELECT = 'uid, nome, data, hora, folheto'
 const QUERY_PAGE = `LIMIT ${PAGE_SIZE} OFFSET ${numeroPagina <= 1 ? 0 : PAGE_SIZE * (numeroPagina - 1)}`
+const QUERY_ORDER = ' ORDER BY nome'
+
 let dbEventos = null
 
 if (nome != '') {
 dbEventos = _db.query(`
 SELECT ${SELECT} FROM evento
 WHERE lower(nome) LIKE lower(?) 
+${QUERY_ORDER}
 ${QUERY_PAGE}
 `,`%${nome}%`)
 } else {
-  dbEventos = _db.query('SELECT '+ SELECT +' FROM evento ' + QUERY_PAGE)
+  dbEventos = _db.query('SELECT '+ SELECT +' FROM evento ' + QUERY_ORDER + ' ' + QUERY_PAGE)
   }
 
 _out.json(
--- netuno-app/server/services/evento/post.js
+++ netuno-app/server/services/evento/post.js
@@ -1,27 +1,27 @@
+const dbEvento = _db.queryFirst('SELECT * FROM evento WHERE lower(nome) = lower(?)',
+  _req.getString('nome')
+)
 
-const dbEvento = _db.get('evento', _req.getString('uid'))
-
-if (!dbEvento) {
-  _header.status(404)
+if (dbEvento) {
+  _header.status(409)
   _out.json(
     _val.map()
-      .set('erro', true)
-      .set('mensagem', 'registro-nao-encontrado')
+    .set('erro', true)
+    .set('mensagem', 'registro-ja-existe')
   )
   _exec.stop()
 }
 
 const nome = _req.getString('nome')
-
 const data = _req.getString('data')
 const hora = _req.getString('hora')
 
 _db.insert(
   'evento',
   _val.map()
-    .set('nome', nome)
-    .set('data', data)
-    .set('hora', hora)
+  .set('nome', nome)
+  .set('data', data)
+  .set('hora', hora)
 )
 
 _out.json(
